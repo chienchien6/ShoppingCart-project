f97d669dec2d5a938d38785fcb267078cfe44dda
Chien Chien <emil62y7@gmail.com>
2024-06-29 15:43:49 +0800

    商品功能實作

---
+++ src/main/java/com/chien/springbootmall/service/ProductService.java
@@ -0,0 +1,9 @@
+package com.chien.springbootmall.service;
+
+import com.chien.springbootmall.model.Product;
+
+
+public interface ProductService {
+
+    Product getProductById(Integer productId);
+}
