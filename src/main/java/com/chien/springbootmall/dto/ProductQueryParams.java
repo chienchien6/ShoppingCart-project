34211fa9035998e7021c0dff409c9287d7673b1b
Chien Chien <emil62y7@gmail.com>
2024-07-09 17:35:15 +0800

    商品功能-分頁Pagination)

--- src/main/java/com/chien/springbootmall/dto/ProductQueryParams.java
+++ src/main/java/com/chien/springbootmall/dto/ProductQueryParams.java
@@ -12,6 +12,26 @@ public class ProductQueryParams {
 
     private String sort;
 
+    private Integer offset;
+
+    private Integer limit;
+
+    public Integer getOffset() {
+        return offset;
+    }
+
+    public void setOffset(Integer offset) {
+        this.offset = offset;
+    }
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+    }
+
     public String getOrderBy() {
         return orderBy;
     }
