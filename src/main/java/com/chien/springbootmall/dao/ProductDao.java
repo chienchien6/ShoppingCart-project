066d9fb25206f534eedc1ce679f8df0083a82f27
Chien Chien <emil62y7@gmail.com>
2024-07-09 20:08:51 +0800

    商品功能-分頁+顯示總筆數

--- src/main/java/com/chien/springbootmall/dao/ProductDao.java
+++ src/main/java/com/chien/springbootmall/dao/ProductDao.java
@@ -13,6 +13,8 @@ public interface ProductDao {
 
     List<Product> getProducts(ProductQueryParams productQueryParams);
 
+    Integer countProduct(ProductQueryParams productQueryParams);
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
